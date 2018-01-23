ada06181ea194bd4cca0f344fdccfb7cde62fb4a
renyang <renyang@rivamed.cn>
2018-01-23 18:12:13 +0800

    fix bug

--- app/controllers/projectmenu_controller.js
+++ app/controllers/projectmenu_controller.js
@@ -23,9 +23,22 @@ let Project = sequelize.define('projects', {
 }, {
     timestamps: false
 });
-exports.testProject = async (ctx,next) =>{
+exports.serachProject = async (ctx,next) =>{
     let array = await Project.findAll();
     ctx.body = {
         array: array
     }
+};
+exports.postProject = async(ctx,next)=>{
+    let name = ctx.query.name;
+    if(!name){
+        throw new ApiError(ApiErrorNames.USER_NOT_EXIST);
+    }
+    let createPerson = 'Ren';
+    let now = Date.now();
+    await Project.create({
+        name:name,
+        createPerson:createPerson,
+        createDate:now
+    })
 };
--- routes/api/projectmenu_router.js
+++ routes/api/projectmenu_router.js
@@ -1,6 +1,7 @@
 const router = require('koa-router')();
 const projectmenu_controller = require('../../app/controllers/projectmenu_controller');
 
-router.get('/getProjectMenu', projectmenu_controller.testProject);
+router.get('/getProjectMenu', projectmenu_controller.serachProject);
+router.post('/postProjectMenu', projectmenu_controller.postProject);
 
 module.exports = router;
